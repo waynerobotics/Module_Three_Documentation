2e36813ae856936ea30aa03237e60e0efb371704
hannab8 <83167499+hannab8@users.noreply.github.com>
2023-08-18 15:39:50 -0400

    Python File - mod2

---
+++ Mod2_python.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    num3 = float(input("Enter the third number: "))
+    num4 = float(input("Enter the fourth number: "))
+
+    average = (num1 + num2 + num3 + num4) / 4
+
+    print(f"The average of {num1}, {num2}, {num3}, and {num4} is: {average:.2f}")
